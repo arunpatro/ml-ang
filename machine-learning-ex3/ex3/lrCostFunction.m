56d55c2d0b3335f51bee3d4a0dd3e52d6db5838d
Arun Patro <arun.kumar.patro@gmail.com>
2016-08-12 17:36:55 +0530

    complete regularized logistic regression cost function and gradient

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -37,6 +37,10 @@ grad = zeros(size(theta));
 %
 
 
+h = logsig(X*theta)
+theta2 = theta(2:end)
+J = (1/m)*(-y'*log(h)-(1-y)'*log(1-h)) + (lambda/(2*m))*(theta2'*theta2)
+grad = (1/m)*(X'*(h-y)+(lambda)*[0;theta2]);
 
 
 
